866dea3c60b799fdcf666455139108051de5a370
Bibin Syamnath <bibinsyamnath@hotmail.com>
2021-01-14 17:47:20 +0530

    javafx

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module io.github.bibinsyamnath.javafxiconbrowser {
+    requires javafx.controls;
+    requires transitive javafx.graphics;
+
     exports io.github.bibinsyamnath.javafxiconbrowser;
 }
